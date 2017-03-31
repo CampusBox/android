ae11d49a5d47ef42ea8e082081e055ee93f28720
anurag145 <anurag145@gmail.com>
2017-04-01 00:04:46 +0530

    Custom Button

---
+++ CampusBox/app/src/main/java/github/com/anurag145/campusbox/HOME.java
@@ -0,0 +1,19 @@
+package github.com.anurag145.campusbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class HOME extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+    public void onClick(View view)
+    {
+
+    }
+
+}

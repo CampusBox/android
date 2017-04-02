89dad4b63fb82a9e25b7bc74330cbe092c8905e8
anurag145 <anurag145@gmail.com>
2017-04-02 11:35:50 +0530

    partition home screen

--- CampusBox/app/src/main/java/github/com/anurag145/campusbox/HOME.java
+++ CampusBox/app/src/main/java/github/com/anurag145/campusbox/HOME.java
@@ -2,16 +2,22 @@ package github.com.anurag145.campusbox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.View;
 
 public class HOME extends AppCompatActivity {
-
+  private int count=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
-    public void onClick(View view)
+    public void onClick(View view) {
+
+
+    }
+    public void onClickLogin(View view)
     {
 
     }

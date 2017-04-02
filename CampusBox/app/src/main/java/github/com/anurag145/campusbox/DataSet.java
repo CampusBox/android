dca932b4297aec41148c3020217a4f5f2843f28b
anurag145 <anurag145@gmail.com>
2017-04-02 14:56:35 +0530

    initial structure set up

---
+++ CampusBox/app/src/main/java/github/com/anurag145/campusbox/DataSet.java
@@ -0,0 +1,38 @@
+package github.com.anurag145.campusbox;
+
+import java.util.HashMap;
+
+/**
+ * Created by Anurag145 on 4/2/2017.
+ */
+
+public class DataSet {
+
+    public HashMap<Integer,Integer> list=new HashMap<>();
+    DataSet()
+    {
+        list.put(R.id.rl1,R.id.t1);
+        list.put(R.id.rl2,R.id.t2);
+        list.put(R.id.rl3,R.id.t3);
+        list.put(R.id.rl4,R.id.t4);
+        list.put(R.id.rl5,R.id.t5);
+        list.put(R.id.rl6,R.id.t6);
+        list.put(R.id.rl7,R.id.t7);
+        list.put(R.id.rl8,R.id.t8);
+        list.put(R.id.rl9,R.id.t9);
+        list.put(R.id.rl10,R.id.t10);
+        list.put(R.id.rl11,R.id.t11);
+        list.put(R.id.rl12,R.id.t12);
+        list.put(R.id.rl13,R.id.t13);
+        list.put(R.id.rl14,R.id.t14);
+        list.put(R.id.rl15,R.id.t15);
+        list.put(R.id.rl16,R.id.t16);
+        list.put(R.id.rl17,R.id.t17);
+        list.put(R.id.rl18,R.id.t18);
+        list.put(R.id.rl19,R.id.t19);
+        list.put(R.id.rl20,R.id.t20);
+        list.put(R.id.rl21,R.id.t21);
+    }
+
+
+}
